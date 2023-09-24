c3ef4a3e087785235301539d41ef73f7173d54cf
Anastasiia <kor888inska@gmail.com>
2023-09-24 22:14:47 +0200

    switch

---
+++ study.js
@@ -0,0 +1,25 @@
+function getSubscriptionPrice(type) {
+    let price;
+
+    switch (type) {
+        case "starter":
+            price = 0;
+            breake;
+
+        case "professional":
+            price = 20;
+            breake;
+
+        case "organization":
+            price = 50;
+            breake;
+    }
+
+    return price;
+}
+
+let office = 50;
+
+message = 60 > office;
+
+alert = "Hello";
